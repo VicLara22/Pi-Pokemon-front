dfcdfebba6023a5d17552947a54b88b2d1862931
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-03-25 20:59:45 -0300

    Update Card.jsx

--- client/src/componentes/Card/Card.jsx
+++ client/src/componentes/Card/Card.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styles from './Card.module.css';
 
-export default function Card({name, img, type, id}){
+export default function Card({name, img, types, id}){
     return(
         <div key={id} className={styles.card}>
             <h2>{name[0].toUpperCase() + name.slice(1)}</h2>
             <img src={img} alt='img not found' />
             <div>
-                {type?.map(e => {
-                    return <h4 key={e} value={e} className={styles.type}>{e[0].toUpperCase() + e.slice(1)}</h4>
-                })}
+                 <h3 className={styles.type} >{types}</h3>
+                //{type?.map(e => {
+                  //  return <h4 key={e} value={e} className={styles.type}>{e[0].toUpperCase() + e.slice(1)}</h4>
+                //})}
             
             </div>
           
