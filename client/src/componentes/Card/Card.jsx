635c8c7e1bda5a9260c52a6a2a2319f45d5d9ed7
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-03-25 21:09:49 -0300

    Update Card.jsx

--- client/src/componentes/Card/Card.jsx
+++ client/src/componentes/Card/Card.jsx
@@ -8,10 +8,6 @@ export default function Card({name, img, types, id}){
             <img src={img} alt='img not found' />
             <div>
                  <h3 className={styles.type} >{types}</h3>
-                //{type?.map(e => {
-                  //  return <h4 key={e} value={e} className={styles.type}>{e[0].toUpperCase() + e.slice(1)}</h4>
-                //})}
-            
             </div>
           
         </div>
