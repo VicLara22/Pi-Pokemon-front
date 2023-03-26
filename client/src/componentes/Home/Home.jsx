a21d494b65266e35eb99196223887b26c7f27c89
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-03-25 21:18:27 -0300

    Update Home.jsx

--- client/src/componentes/Home/Home.jsx
+++ client/src/componentes/Home/Home.jsx
@@ -125,7 +125,7 @@ export default function Home() {
               currentPokemons?.map((e) =>
                 <Link to={`/home/${e.id}`}>
                   <Card id={e.id} name={e.name} img={e.img} 
-                    type={e.types?.map((el, index) => el.name.charAt(0).toUpperCase() + el.name.slice(1) + (index === e.types.length - 1 ? '' : ', '))}
+                    types={e.types?.map((el, index) => el.name.charAt(0).toUpperCase() + el.name.slice(1) + (index === e.types.length - 1 ? '' : ', '))}
                   // type={e.type}
                   key={e.id} />   
                   </Link>)
