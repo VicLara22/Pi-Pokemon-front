51939e8079a22006c6dac8a391d1eff06e9e2d79
Lara Moyano <99929493+VicLara22@users.noreply.github.com>
2023-03-25 21:50:38 -0300

    Update Detail.jsx

--- client/src/componentes/Detail/Detail.jsx
+++ client/src/componentes/Detail/Detail.jsx
@@ -38,11 +38,11 @@ export default function Detail() {
                             <h4>Speed: {detail[0].speed}</h4>
                             <h4>Height: {detail[0].height}</h4>
                             <h4>Weight: {detail[0].weight}</h4>
-                            <h3>Types: {detail[0].type?.map(e => {
+                            <h3>Types: {detail[0]?.types?.map(e => {
                                     return <h3 key={e} value={e} className={styles.type}>{e[0].toUpperCase()+ e.slice(1)}</h3>
                              })} </h3>
 
-{/*                              <h3>Types: {detail[0].types[0].name} </h3>
+{/*                           <h3>Types: {detail[0].types[0].name} </h3>
  */}                              
 
                             </div>
